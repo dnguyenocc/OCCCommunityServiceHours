a505fbf5417b505ee9272f7f07abe7e96fe51aff
bijan102 <bfazeli1@gmail.com>
2016-11-24 13:59:04 -0800

    Created a class called Event that will be used to instantiate new events and declared private fields.

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/dnguyen1214/occcommunityservicehours/Event.java
@@ -0,0 +1,12 @@
+package edu.orangecoastcollege.cs273.dnguyen1214.occcommunityservicehours;
+
+/**
+ * Created by bijanfazeli on 11/24/16.
+ */
+
+public class Event {
+    // Declare private fields
+    private int mId, mDay, mMonth, mYear, mHours;
+    private String mName, mDescription, mLocation, mCondition, mStartTime, mEndTime;
+
+}
