9aaf136d16fa57787ec834e3ba351589ffba738a
bijan102 <bfazeli1@gmail.com>
2016-12-01 16:57:42 -0800

    Updated and implemented an event loadFragment to load the respective fragment based off the navigation item selected.

--- app/src/main/java/edu/orangecoastcollege/cs273/dnguyen1214/occcommunityservicehours/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dnguyen1214/occcommunityservicehours/MainActivity.java
@@ -1,32 +1,22 @@
 package edu.orangecoastcollege.cs273.dnguyen1214.occcommunityservicehours;
 
-import android.annotation.TargetApi;
-import android.app.SearchManager;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener,
-        MainFragment.OnFragmentInteractionListener {
-
-    SessionManager manager;
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,24 +24,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //Hide the title on app bar for Search View
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        manager = new SessionManager();
-        //Set the main fragment
-        if (savedInstanceState == null) {
-            Fragment fragment = null;
-            Class fragmentClass = null;
-            fragmentClass = MainFragment.class; // change here
-            try {
-                fragment = (Fragment) fragmentClass.newInstance();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
-        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -65,11 +37,14 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Load a fragment onto the layout when onCreate is made
+        loadFragment(new MainFragment());
     }
 
     @Override
@@ -79,42 +54,13 @@ public class MainActivity extends AppCompatActivity
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
-            Intent intent = new Intent(Intent.ACTION_MAIN);
-            intent.addCategory(Intent.CATEGORY_HOME);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//***Change Here***
-            startActivity(intent);
-            finish();
-            System.exit(0);
         }
     }
 
     @Override
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-
-
-        SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-
-        SearchView search = (SearchView) menu.findItem(R.id.menu_search).getActionView();
-
-        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                //TODO write your code what you want to perform on search
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //TODO write your code what you want to perform on search text change
-                return false;
-            }
-        });
-
-
-
         return true;
     }
 
@@ -133,53 +79,32 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        Fragment fragment = null;
-        Class fragmentClass = null;
-
-        if (id == R.id.nav_profile) {
-            fragmentClass = MainFragment.class;// transition fragment
-            transitionFragment(fragment,fragmentClass);
 
 
-        } else if (id == R.id.nav_attending_events) {
-            //TODO put fragment want to be transition here
-        } else if (id == R.id.nav_upcoming_events) {
-            //TODO put fragment want to be transition here
-        } else if (id == R.id.nav_passed_events) {
-            //TODO put fragment want to be transition here
-        } else if (id == R.id.nav_share) {
-            //TODO put fragment want to be transition here
-        } else if (id == R.id.nav_help) {
-            //TODO put fragment want to be transition here
-        }else if (id == R.id.nav_exist) {
-            manager.setPreferences(MainActivity.this, "status", "0");
-            finish();
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
-
+        if (id == R.id.nav_upcoming_events) {
+            // Handle the camera action
+            setTitle("Account Details Fragment");
+            loadFragment(new UpcommingEventsListActivityFragment());
         }
+        else if (id == R.id.nav_attending_events) {
+            setTitle("Attended Events Fragment");
+            loadFragment(new MainFragment());
+        }
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-    public void transitionFragment(Fragment fragment, Class fragmentClass)
-    {
-        try {
-            fragment = (Fragment) fragmentClass.newInstance();
-            // Insert the fragment by replacing any existing fragment
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    @Override
-    public void onFragmentInteraction(Uri uri) {
 
+    private void loadFragment (Fragment frag)
+    {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.flContent, frag, "Account Details");
+        fragmentTransaction.commit();
     }
 }
