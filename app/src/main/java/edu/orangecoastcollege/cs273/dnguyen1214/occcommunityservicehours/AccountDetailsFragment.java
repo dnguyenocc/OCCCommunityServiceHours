b90f3015701b6eeec2340d20f438429acc85b80f
bijan102 <bfazeli1@gmail.com>
2016-12-05 12:41:13 -0800

    Completed Selecting asking for permissions and selecting an image in AccountDetailsFragment.

--- app/src/main/java/edu/orangecoastcollege/cs273/dnguyen1214/occcommunityservicehours/AccountDetailsFragment.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dnguyen1214/occcommunityservicehours/AccountDetailsFragment.java
@@ -2,9 +2,14 @@ package edu.orangecoastcollege.cs273.dnguyen1214.occcommunityservicehours;
 
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
@@ -14,6 +19,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -22,6 +28,10 @@ import java.util.ArrayList;
  * A simple {@link Fragment} subclass.
  */
 public class AccountDetailsFragment extends Fragment implements View.OnClickListener{
+
+    // Request codes
+    private static final int REQUEST_CODE_PHOTO = 100;
+    private static final int REQUEST_CODE_CAMERA = 101;
 
     // Declare local variables
     private Button submitButton;
@@ -34,6 +44,7 @@ public class AccountDetailsFragment extends Fragment implements View.OnClickList
 
     private User loginUser;
     private DBHelper db;
+    private Uri imageUri;
 
     public AccountDetailsFragment() {
         // Required empty public constructor
@@ -55,9 +66,6 @@ public class AccountDetailsFragment extends Fragment implements View.OnClickList
 
         // Display the User's info
         setUp (v, loginUser);
-
-
-
 
         return v;
     }
@@ -103,22 +111,76 @@ public class AccountDetailsFragment extends Fragment implements View.OnClickList
         // List of all the permissions to request from user
         ArrayList<String> permList = new ArrayList<>();
 
+        // Check to see if permissions to camera has been granted
+        int cameraPermission = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA);
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED)
+            permList.add(Manifest.permission.CAMERA);
+
         // Check if permissions to camera is granted
-        int readExternalStoragePermission = ContextCompat.checkSelfPermission(mContext, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int readExternalStoragePermission = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
         if (readExternalStoragePermission != PackageManager.PERMISSION_GRANTED)
             permList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
 
-        // Check if permissions to writing external store is granted
-        int writeExternalStoragePermission = ContextCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        // Check if permissions to writing external storage is granted
+        int writeExternalStoragePermission = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
         if (writeExternalStoragePermission != PackageManager.PERMISSION_GRANTED)
             permList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        // If the list has items (size > 0), request permissions from the user:
+        if (permList.size() > 0)
+        {
+            // Convert ArrayList into an Array of Strings
+            String[] perms = new String[permList.size()];
+            // Request permission from user
+            ActivityCompat.requestPermissions(getActivity(), permList.toArray(perms), REQUEST_CODE_PHOTO);
+        }
+
+        // If we have all 3 permissions, open ImageGallery
+        if (cameraPermission == PackageManager.PERMISSION_GRANTED
+                && readExternalStoragePermission == PackageManager.PERMISSION_GRANTED
+                && writeExternalStoragePermission == PackageManager.PERMISSION_GRANTED)
+        {
+            // Start an intent to launch gallery and take pics
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, REQUEST_CODE_PHOTO);
+        } else {
+            Toast.makeText(getContext(),
+                    "OCC Community Service Hours requires camera and external storage permission",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * Receive the result from a previous call to
+     * {@link #startActivityForResult(Intent, int)}.  This follows the
+     * related Activity API as described there in
+     * {@link Activity#onActivityResult(int, int, Intent)}.
+     *
+     * @param requestCode The integer request code originally supplied to
+     *                    startActivityForResult(), allowing you to identify who this
+     *                    result came from.
+     * @param resultCode  The integer result code returned by the child activity
+     *                    through its setResult().
+     * @param data        An Intent, which can return result data to the caller
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data != null && requestCode == REQUEST_CODE_PHOTO && resultCode == Activity.RESULT_OK)
+        {
+            // Set the imageURI to the data
+            imageUri = data.getData();
+            profileImageView.setImageURI(imageUri);
+        }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.profileImageView:
-
+                selectProfileImage(v);
+                break;
         }
     }
 }
