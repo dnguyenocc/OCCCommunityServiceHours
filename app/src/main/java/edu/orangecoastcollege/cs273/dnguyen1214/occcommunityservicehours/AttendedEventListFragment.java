847a17359db05f0742c0e6dbb56b815387205fbe
bijan102 <bfazeli1@gmail.com>
2016-12-06 12:49:34 -0800

    Update

--- app/src/main/java/edu/orangecoastcollege/cs273/dnguyen1214/occcommunityservicehours/AttendedEventListFragment.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dnguyen1214/occcommunityservicehours/AttendedEventListFragment.java
@@ -23,13 +23,13 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AttendedEventListFragment extends Fragment {
+public class AttendedEventListFragment extends ListFragment {
 
     private DBHelper db;
     private List<Participation> allParticipationList;
-    private List<Event> allUserEvents;
+    private List<Event> pastEvents;
 
-    private ListView allEventsListView;
+    private ListView list;
 
     private EventListAdapter eventsListAdapter;
 
@@ -40,36 +40,48 @@ public class AttendedEventListFragment extends Fragment {
     }
 
 
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_attended_event_list, container, false);
-        db = new DBHelper(getContext());
 
-
-        allEventsListView = (ListView) v.findViewById(R.id.allEventsListView);
-
-
-        user = db.getLoginUser();
-        allParticipationList = db.getAllParticipationsByUserId(user.getmId());
-
-
-        allUserEvents = new ArrayList<Event>();
-        for (Participation participation : allParticipationList) {
-            if (participation.getEvent().eventPassed()) allUserEvents.add(participation.getEvent());
-        }
-
-        if (allUserEvents == null)
-            Toast.makeText(getContext(), "", Toast.LENGTH_SHORT).show();
-        eventsListAdapter = new EventListAdapter(getContext(), R.layout.event_list_item, allUserEvents);
-        allEventsListView.setAdapter(eventsListAdapter);
 
 
 
         return v;
     }
 
+    /**
+     * Attach to list view once the view hierarchy has been created.
+     *
+     * @param view
+     * @param savedInstanceState
+     */
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        db = new DBHelper(getContext());
+
+
+        list = getListView();
+
+
+        user = db.getLoginUser();
+        allParticipationList = db.getAllParticipationsByUserId(user.getmId());
+
+
+        pastEvents = new ArrayList<Event>();
+        for (Participation participation : allParticipationList) {
+            if (participation.getEvent().eventPassed()) pastEvents.add(participation.getEvent());
+        }
+
+        if (pastEvents == null)
+            Toast.makeText(getContext(), "", Toast.LENGTH_SHORT).show();
+        eventsListAdapter = new EventListAdapter(getContext(), R.layout.event_list_item, pastEvents);
+        list.setAdapter(eventsListAdapter);
+
+    }
 
     /**
      * This method will be called when an item in the list is selected.
@@ -82,16 +94,17 @@ public class AttendedEventListFragment extends Fragment {
      * @param position The position of the view in the list
      * @param id       The row id of the item that was clicked
      **/
-    /* MUST FIX LATER, THIS IS WRONG
+    //MUST FIX LATER, THIS IS WRONG
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
 
+
+
         Bundle bundle1 = new Bundle();
-        Event selectedEvent = (Event) v.getTag();
-        bundle1.putParcelable("SelectedEvent", selectedEvent);
-        Intent intent = new Intent(getActivity(), EventDetailsActivity.class);
+        bundle1.putParcelable("SelectedEvent", pastEvents.get(position));
+        Intent intent = new Intent(getContext(), EventDetailsActivity.class);
         intent.putExtras(bundle1);
         getActivity().startActivity(intent);
-    }*/
+    }
 }
--- app/src/main/java/edu/orangecoastcollege/cs273/dnguyen1214/occcommunityservicehours/AttendingEventListFragment.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dnguyen1214/occcommunityservicehours/AttendingEventListFragment.java
@@ -3,16 +3,29 @@ package edu.orangecoastcollege.cs273.dnguyen1214.occcommunityservicehours;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AttendingEventListFragment extends Fragment {
+public class AttendingEventListFragment extends ListFragment {
 
+    private DBHelper db;
+    private List<Participation> allParticipationList;
+    private List<Event> pastEvents;
+
+    private ListView allEventsListView;
+
+    private EventListAdapter eventsListAdapter;
+
+    private User user;
 
     public AttendingEventListFragment() {
         // Required empty public constructor
