d0f7414e47fe295a482d7f45e2a058e5c4d1d822
Alex Ho <huyho114@gmail.com>
2016-12-08 02:21:50 -0800

    update Ask email and question security activity and layout

--- app/src/main/java/edu/orangecoastcollege/cs273/dnguyen1214/occcommunityservicehours/AnswerQuestionSecurityFragment.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dnguyen1214/occcommunityservicehours/AnswerQuestionSecurityFragment.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link MainFragment.OnFragmentInteractionListener} interface
+ * {@link AnswerQuestionSecurityFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link MainFragment#newInstance} factory method to
+ * Use the {@link AnswerQuestionSecurityFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MainFragment extends Fragment {
+public class AnswerQuestionSecurityFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class MainFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public MainFragment() {
+    public AnswerQuestionSecurityFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class MainFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
+     * @return A new instance of fragment AnswerQuestionSecurityFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static MainFragment newInstance(String param1, String param2) {
-        MainFragment fragment = new MainFragment();
+    public static AnswerQuestionSecurityFragment newInstance(String param1, String param2) {
+        AnswerQuestionSecurityFragment fragment = new AnswerQuestionSecurityFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -54,7 +54,6 @@ public class MainFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -65,7 +64,7 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_answer_question_security, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
